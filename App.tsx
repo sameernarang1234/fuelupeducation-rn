cdb6bc21985eb2e4448e1036e5dc1ed964e025c5
Sayeed Afridi <as.afridi563@gmail.com>
2020-10-27 22:05:56 +0600

    created global theme

--- App.tsx
+++ App.tsx
@@ -1,16 +1,20 @@
 import React, { useEffect } from 'react'
 import { StyleSheet, View, Text, StatusBar } from 'react-native'
 import RNBootSplash from 'react-native-bootsplash'
+import { ThemeProvider } from '@shopify/restyle'
+import theme from './src/Theme'
 
 const App: React.FC = () => {
   useEffect(() => {
     RNBootSplash.hide()
   }, [])
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle='light-content' />
-      <Text>Start editing App.js</Text>
-    </View>
+    <ThemeProvider theme={theme}>
+      <View style={styles.container}>
+        <StatusBar barStyle='light-content' />
+        <Text>Start editing App.js</Text>
+      </View>
+    </ThemeProvider>
   )
 }
 
